937ee3fcf7d9d786d0e5c2b537103f4bae48a4e1
Chris <orj.tsuk@gmail.com>
2022-02-10 08:46:44 -0500

    added titles to the sidebar instead of just calling it note..

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,9 @@
 import React from "react"
 
 export default function Sidebar(props) {
+
+
+
     const noteElements = props.notes.map((note, index) => (
         <div key={note.id}>
             <div
@@ -9,8 +12,10 @@ export default function Sidebar(props) {
                     note.id === props.currentNote.id ? "selected-note" : ""
                 }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
+                
             >
-                <h4 className="text-snippet">Note {index + 1}</h4>
+                <h4 onKeyUp={() => console.log(index)} className="text-snippet">{note.body.split("\n")[0] + index}
+                </h4>
             </div>
         </div>
     ))
